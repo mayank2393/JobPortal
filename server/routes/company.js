fa0d4d5734a7fecc1962e8244b84193a2f104df9
Mayank Tripathi <mayankchamps27@gmail.com>
2025-01-01 12:58:50 +0530

    debugging

--- server/routes/company.js
+++ server/routes/company.js
@@ -6,7 +6,7 @@ import { singleUpload } from "../middlewares/multer.js";
 const router = express.Router();
 
 router.route("/register").post(isAuthenticated, registerCompany);
-router.route("/get").get(isAuthenticated, getCompany);
+router.route("/get").get(getCompany);
 router.route("/get/:id").get(isAuthenticated, getCompanyById);
 router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
 
